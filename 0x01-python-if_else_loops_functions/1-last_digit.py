33fbb72d5d3cd240f97093d00e960440e18c526c
elmaayouf <boubkerelmaayouf@gmail.com>
2023-06-06 11:07:52 -0700

    printing the last digit ✔

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+compare = abs(number) % 10
+str = ""
+if compare > 5 :
+    str = "and is greater than 5"
+elif compare == 0 :
+    str = "and is 0"
+elif compare < 6 and compare != 0:
+    str = "and is less than 6 and not 0"
+
+print(f"Last digit of {number} is {compare} {str}")
