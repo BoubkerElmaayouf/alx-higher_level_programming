1f0ceb796b5ea72c1e2c175a1cbc4b7fc22955ca
elmaayouf <boubkerelmaayouf@gmail.com>
2023-06-08 11:38:20 -0700

    task4: prints all the names defined by the compiled module✔

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import  hidden_4 as hid
+    for x in range(len(dir(hid))):
+        if (dir(hid)[x][:2] == "__"):
+            continue
+        print(dir(hid)[x])
