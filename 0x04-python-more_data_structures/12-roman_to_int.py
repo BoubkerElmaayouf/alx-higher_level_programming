fdc17daa8956819eb8d4902cfbf4e4c95391bdf3
elmaayouf <boubkerelmaayouf@gmail.com>
2023-06-14 17:48:09 -0700

    task12:  converts a Roman numeral to an integer.☄

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,10 +8,10 @@ def roman_to_int(roman_string):
     result = 0
 
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] >
-        roman_dict[roman_string[i - 1]]:
-            result += roman_dict[roman_string[i]] -
-            2 * roman_dict[roman_string[i - 1]]
+        if i > 0 and roman_dict[roman_string[i]] \
+                > roman_dict[roman_string[i - 1]]:
+            result += roman_dict[roman_string[i]] \
+                    - 2 * roman_dict[roman_string[i - 1]]
         else:
             result += roman_dict[roman_string[i]]
 
