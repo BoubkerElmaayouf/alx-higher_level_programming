d7790df71ddfa14b35406a6b91b5d9525da26d4a
elmaayouf <boubkerelmaayouf@gmail.com>
2023-06-26 09:18:51 -0700

    task3: func that prints the first x elements of a list and only integers.✔

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            counter = counter + 1
+        except (TypeError, ValueError):
+            pass
+    print("")
+    return (counter)
