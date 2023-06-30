69bc9cbb5950169b5a4047143ba66664dd79ef18
elmaayouf <boubkerelmaayouf@gmail.com>
2023-06-30 04:05:38 -0700

    task1: definding a class with privite attribute✔

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Definding A Class """
+
+
+class Square:
+    """ Represent A Square """
+
+    def __init__(self, size):
+        """ Initialize A New Square """
+
+        self.__size = size
