2bc482cb1cc37a4fa13f8455876d7993845bd80c
elmaayouf <boubkerelmaayouf@gmail.com>
2023-07-04 13:04:59 -0700

    Task 30: magic_string()☄

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""  function magic_string() that returns a string “BestSchool”
+n times the number of the iteration """
+
+
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return ", ".join(["BestSchool"] * magic_string.count)
