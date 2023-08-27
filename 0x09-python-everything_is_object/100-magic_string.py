16e9036a2debfb46e847369868cbede289f91414
elmaayouf <boubkerelmaayouf@gmail.com>
2023-08-27 08:29:35 -0700

    Removing the 0x09-python-everything_is_object form the repo ✔

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""  function magic_string() that returns a string “BestSchool”
-n times the number of the iteration """
-
-
-def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ", ".join(["BestSchool"] * magic_string.count)
