7366d0ad6976150cb5ff4be9f6952929c9c58e4f
BoubkerElmaayouf <boubkerelmaayouf@gmail.com>
2023-09-07 06:27:40 -0700

    Task 2 : Update ☄

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,23 @@
-#!/uer/bin/python3
-""" Define a function that say my first and last name"""
+#!/usr/bin/python3
+
+""" Define a function that says my first and last name"""
+
 
 def say_my_name(first_name, last_name=""):
     """
     Say this first and last name
 
     Args:
-        first and last names
+        first_name: First name
+        last_name: Last name (optional)
 
     Raises:
-        TypeError: if the last and first
-                    name is not string
+        TypeError: if first_name or last_name is not a string
     """
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("my name is {} {}".format(first_name, last_name))
+
+    print("My name is {} {}".format(first_name, last_name))
