d535a422cb2761e9ff9a4c97f40269f55b8dd0f8
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2023-11-30 09:28:29 -0800

    Almost all the tasks ⚡

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" The Rectangle Class """
+
+
+class Rectangle:
+    """Definding: class retangle"""
+
+    def __init__(self, width=0, height=0):
+        """ Init A New Retangle
+        Args: NTH
+        Width: int
+        Height: int
+        Rasie: TypeError if not intiger
+        ValueError: if negative
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getting the value of the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting or Update the value of the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getting the value of the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting the value of  the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" The Rectangle class """
+
+
+class Rectangle:
+    """ Definding: Rectanle class """
+
+    def __init__(self, width=0, height=0):
+        """Start A New Retangle
+        Args:
+        width: intiger
+        height: initiger
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getting  the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting  the width of the retangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the retangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Return the area of the retangel"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the retangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return the printable representation of the rectangle
+        represent rectangel with #
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+
+        r = []
+        for i in range(self.height):
+            [r.append('#') for k in range(self.width)]
+            if i != self.height - 1:
+                r.append("\n")
+        return "".join(r)
