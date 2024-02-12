42107d295094e918372fb89ea245cfc1b911f658
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-02-12 13:12:37 -0800

    printing mullti line ✨

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-const args = process.argv.length;
-const x = parseInt(process.argv[2]);
-if (args !== 3) {
-  console.log('Missing number of occurrences');
-} else {
-  if (!isNaN(x) && x > 0) {
-    for (let i = 0; i < x; i++) {
-      console.log('C is fun');
-    }
-  } else {
-    console.log('Missing number of occurrences');
+const { argv } = require('process');
+const Str = 'C is fun';
+let x = parseInt(argv[2], 10);
+if (!isNaN(x)) {
+  while (x > 0) {
+    console.log(Str);
+    x--;
   }
-}
+} else console.log('Missing number of occurrences');
