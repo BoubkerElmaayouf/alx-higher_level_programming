5844566cefdd5c46fd72c3156a923998dcb0db68
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-02-12 16:54:17 +0100

    arguments in js ✔

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argslen = process.argv.length;
+
+if (argslen === 2) {
+  console.log('No argument');
+} else if (argslen === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
