3deac10213f70b35e94f364a66fa2a56addc48a5
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-02-14 01:00:44 +0100

    printing rectangle ✨

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str = str + 'X';
+      }
+      console.log(str);
+    }
+  }
+}
+
+module.exports = Rectangle;
