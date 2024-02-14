1cc17e377190eaed2fb79bef7c123b5672d46e24
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-02-14 23:00:35 +0100

    reversing a list ✨

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const emptyList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    emptyList.push(list[i]);
+  }
+  return emptyList;
+};
