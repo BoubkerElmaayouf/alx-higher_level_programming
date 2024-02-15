3eb7e3e5360107434216664acfca252d2838e954
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-02-16 00:03:49 +0100

    select_state ✨

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+states models
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db_host = "localhost"
+    db_user = sys.argv[1]
+    db_password = sys.arv[2]
+    db_name = sys.argv[3]
+    port = 3306
+
+    db = MySQLdb.connect(
+        host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
+    )
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+
+    for rows in row:
+        print(rows)
+    cursor.close()
+    db.close()
