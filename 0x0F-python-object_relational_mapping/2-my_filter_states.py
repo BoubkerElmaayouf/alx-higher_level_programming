44e1070fbcbf61b0e252c0ee5c422c481ff1e5bb
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-02-25 19:37:28 +0100

    my filter state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-import cmd
+"""states models
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-class Mycommand(cmd.Cmd):
-    prompt = "ourcommand >"
-    
-    def do_quit(self, arg):
-        """ This will quit the program!!"""
-        return True
-    
-    # aliasing
-    do_exit = do_quit
-Mycommand().cmdloop()
+    db_host = "localhost"
+    db_user = sys.argv[1]  # "your_username"
+    db_password = sys.argv[2]  # "your_password"
+    db_name = sys.argv[3]  # "your_database_name"
+    port = 3306
+    state_name = sys.argv[4]  # "your_database_name"
+    query = "SELECT * FROM states WHERE name LIKE BINARY\
+ '{}' ORDER BY id ASC".format(state_name)
+    db = MySQLdb.connect(
+        host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
+    )
+    cursor = db.cursor()
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
