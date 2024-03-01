e8c48f663f08808fff7039621a083586237d8af6
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-03-01 22:51:47 +0100

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module document """
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    result = requests.get(url)
+    print(result.headers.get("X-Request-Id"))
