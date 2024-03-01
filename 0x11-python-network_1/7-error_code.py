a60e4b7f74fbcaf0051dde58d71452ccf2811ca3
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-03-01 22:55:47 +0100

    task 7: error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" module doc """
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    result = requests.get(url)
+    if (result.status_code >= 400):
+        print(f"Error code: {result.status_code}")
+        exit()
+    print(result.text)
