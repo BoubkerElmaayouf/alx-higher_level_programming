dcbabdfc6fedb41e4e8825f30a9e88eb5159109b
boubkerelmaayouf <boubkerelmaayouf@gmail.com>
2024-04-09 02:42:37 +0000

    multiline using for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const myarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i =0 ; i < myarray.length;i++){
-    console.log(myarray[i]);
+for (let i = 0; i < myarray.length; i++) {
+  console.log(myarray[i]);
 }
